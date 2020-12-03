4398246cd31884c28e8a829d30de32d37018e035
Gokul Ramalingam <gokul821997@gmail.com>
2020-12-03 09:05:15 +0530

    Initial project creation using Spring starter project.

---
+++ OrderMyFood/src/main/java/com/omf/OrderMyFoodApplication.java
@@ -0,0 +1,13 @@
+package com.omf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderMyFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderMyFoodApplication.class, args);
+	}
+
+}
